42aa0a9a3e56cb8f9fbc22d465e8c4db6d88b58d
Nopfed <robby.tsunami@gmail.com>
2025-11-06 12:54:23 -0500

    Adds map gen, exit and player placements

--- game.gd
+++ game.gd
@@ -4,15 +4,17 @@ extends Node2D
 
 @export var noiseTexture: NoiseTexture2D
 
+const TILE_SIZE := 16
 const MAX_MAP_WIDTH := 100
 const MAX_MAP_HEIGHT := 100
 const MIN_MAP_WIDTH := 20
 const MIN_MAP_HEIGHT := 20
-const WATER_HEIGHT := -0.6
-const FLOOR_HEIGHT := 0.6
-#const WALL_HEIGHT := 1.0
+const WATER_HEIGHT := -0.45
+const FLOOR_HEIGHT := 0.25
+const WALL_HEIGHT := 1.0
 
 var noise: Noise
+
 
 func _ready() -> void:
 	newGame()
@@ -41,36 +43,36 @@ func drawMap():
 	for x in range(-mapWidth, mapWidth):
 		for y in range(-mapHeight, mapHeight):
 			noiseValue = noise.get_noise_2d(x, y)
-			print(noiseValue)
+			
 			# if there is not a pre-drawn cell
 			if $TileMapLayer.get_cell_source_id(Vector2(x, y)) < 0:
-				# Draw Walls
-				if noiseValue > FLOOR_HEIGHT:
-					$TileMapLayer.set_cell(
-						Vector2(x, y),
-						0,
-						Vector2(0, 0)
-					)
-				# Draw floor
-				elif noiseValue > WATER_HEIGHT:
-					floorTiles.append(Vector2(x, y))
-					$TileMapLayer.set_cell(
-						Vector2(x, y),
-						0,
-						Vector2(2, 0)
-					)
 				# Draw water
-				else:
-					$TileMapLayer.set_cell(
-						Vector2(x, y),
-						0,
-						Vector2(5, 0)
-					)
-	var exitCoords = floorTiles.pick_random()
+				if noiseValue < WATER_HEIGHT:
+					$TileMapLayer.set_cell(Vector2(x, y), 0, Vector2(5, 0))
+				# Draw floor
+				elif noiseValue < FLOOR_HEIGHT:
+					floorTiles.append(Vector2(x, y))
+					$TileMapLayer.set_cell(Vector2(x, y), 0, Vector2(2, 0))
+				else: # Draw Walls
+					if noiseValue > FLOOR_HEIGHT:
+						$TileMapLayer.set_cell(Vector2(x, y), 0, Vector2(0, 0))
+			
+			# Draw Borders
+			if x == -mapWidth or x == mapWidth - 1:
+				$TileMapLayer.set_cell(Vector2(x, y), 0, Vector2(0, 0))
+			if y == -mapHeight or y == mapHeight - 1:
+				$TileMapLayer.set_cell(Vector2(x, y), 0, Vector2(0, 0))
 	
-	$TileMapLayer.set_cell(exitCoords, 0, Vector2(0, 8))
+	floorTiles.shuffle()
 	
+	var exitCoords = floorTiles.pop_back()
 	var exit = exitScene.instantiate()
 	
-	exit.position = exitCoords * 16
+	exit.position = exitCoords * TILE_SIZE
+	$TileMapLayer.set_cell(exitCoords, 0, Vector2(0, 8))
 	add_child(exit)
+	
+	var playerPosition = floorTiles.pop_back()
+	
+	$Player.position = playerPosition * TILE_SIZE
+	
