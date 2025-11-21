af327ee6df9fd1ab328774898770a894af61536e
Nopfed <robby.tsunami@gmail.com>
2025-11-20 23:20:01 -0500

    WIP: Armory system

--- armory.gd
+++ armory.gd
@@ -1,1 +1,18 @@
 extends Node
+
+const WEAPONS = {
+	'sword': [
+		'rapier',
+		'scimitar',
+		'sword',
+		'shortsword',
+		'machete',
+		'blade',
+		'sabre',
+		'gladius',
+		'clipper',
+		'skinner',
+		'broadsword',
+		'cutlass'
+	]
+}
