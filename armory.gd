d8ca2324ecd9d6dd5108387f9379904e6a8777b8
Nopfed <robby.tsunami@gmail.com>
2025-11-25 03:23:16 -0500

    WIP: Armory

--- armory.gd
+++ armory.gd
@@ -18,18 +18,123 @@ const RARITIES: Array[String] = [
 ]
 
 const WEAPONS = {
-	'sword': [
-		'rapier',
-		'scimitar',
-		'sword',
-		'shortsword',
-		'machete',
-		'blade',
-		'sabre',
-		'gladius',
-		'clipper',
-		'skinner',
-		'broadsword',
-		'cutlass'
-	]
+	'rapier': {
+		'damage': [2, 4],
+		'attributes': ['dexterity', 'charisma']
+	},
+	'scimitar': {
+		'damage': [3, 6],
+		'attributes': ['constitution', 'strength', 'charisma']
+	},
+	'sword': {
+		'damage': [2, 5],
+		'attributes': [
+			'intelligence',
+			'strength',
+			'constitiution',
+			'dexterity',
+			'wisdom',
+			'charisma'
+		]
+	},
+	'shortsword': {
+		'damage': [2, 4],
+		'attributes': [
+			'intelligence',
+			'strength',
+			'constitiution',
+			'dexterity',
+			'wisdom',
+			'charisma'
+		]
+	},
+	'machete': {
+		'damage': [3, 5],
+		'attributes': ['strength', 'dexterity']
+	},
+	'blade': {
+		'damage': [3, 4],
+		'attributes': [
+			'strength',
+			'constitiution',
+			'dexterity',
+			'wisdom',
+			'charisma'
+		]
+	},
+	'sabre': {
+		'damage': [2, 5],
+		'attributes': [
+			'strength',
+			'constitiution',
+			'dexterity',
+			'charisma'
+		]
+	},
+	'gladius': {
+		'damage': [3, 5],
+		'attributes': [
+			'strength',
+			'constitiution',
+			'dexterity',
+			'wisdom',
+			'charisma'
+		]
+	},
+	'broadsword': {
+		'damage': [3, 5],
+		'attributes': [
+			'strength',
+			'constitiution',
+			'dexterity',
+			'wisdom',
+			'charisma'
+		]
+	},
+	'cutlass': {
+		'damage': [3, 5],
+		'attributes': [
+			'intelligence',
+			'strength',
+			'constitiution',
+			'dexterity',
+			'wisdom',
+			'charisma'
+		]
+	}
+}
+
+const ARMOR = {
+	'chest': {
+		'material': [
+			'leather',
+			'studded',
+			'cloth',
+			'steel',
+			'iron',
+			'bronze',
+			'copper',
+			'silver',
+			'gold'
+		],
+		'fashion': [
+			'hauberk',
+			'cuirass',
+			'brigandine',
+			'plackart',
+			'faulds',
+			'lorica'
+		]
+	}
+}
+
+const MATERIALS = {
+	'leather': {
+		'attributes': [
+		]
+	},
+	'studded': {
+		'attributes': [
+		]
+	}
 }
--- scenes/item/item.gd
+++ scenes/item/item.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 var type: String
 var rarity: String
+var stats: Dictionary
 
 
 func _ready() -> void:
@@ -20,4 +21,6 @@ func rollRarity():
 
 
 func rollStats():
+	# TODO -> Get item stats
+	# TODO -> Create item name based on it's stats and rarity
 	pass
